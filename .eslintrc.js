c93c0e8a5514f9b02a6768b3fd5e867abcd58095
zieraell <roobydemongirl@gmail.com>
2023-09-21 04:44:03 -0400

    File Setup
    - Added listed settings.json
    
    - Generated .eslintrc.js
    
    - Added .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+/* eslint-disable quote-props */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-trailing-spaces': 'error',
+    'semi': 'error',
+    'linebreak-style': ['error', 'unix'],
+  },
+};
